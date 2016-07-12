7394192eacca42a3d07f0ad2e6013d0a0495e17b
tugrul <tugrulelmas@gmail.com>
2016-07-12 09:10:12 +0300

    decorator

---
+++ src/Patterns/Decorator.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Patterns
+{
+    public interface IPizza
+    {
+        string Description { get; }
+
+        double Cost { get; }
+
+        IEnumerable<PizzaType> Types { get; }
+    }
+
+    public class PlainPizza : IPizza
+    {
+        public double Cost { get { return 2; } }
+
+        public string Description { get { return "Standart"; } }
+
+        public IEnumerable<PizzaType> Types {
+            get { return new List<PizzaType> { PizzaType.Plain }; }
+        }
+    }
+
+    public abstract class ToppingDecorator : IPizza
+    {
+        protected readonly IPizza pizza;
+
+        public ToppingDecorator(IPizza pizza) {
+            this.pizza = pizza;
+        }
+
+        protected abstract PizzaType PizzaType { get; }
+
+        public virtual double Cost { get { return pizza.Cost; } }
+
+        public virtual string Description { get { return $"{pizza.Description}, {PizzaType}"; } }
+
+        public virtual IEnumerable<PizzaType> Types {
+            get {
+                var result = pizza.Types.ToList();
+                result.Add(PizzaType);
+                return result;
+            }
+        }
+    }
+
+    public class Mozzeralla : ToppingDecorator
+    {
+        public Mozzeralla(IPizza pizza)
+            : base(pizza) {
+        }
+
+        public override double Cost { get { return base.Cost + 0.5; } }
+
+        protected override PizzaType PizzaType { get { return PizzaType.Mozzarella; } }
+    }
+
+    public class Ham : ToppingDecorator
+    {
+        public Ham(IPizza pizza)
+            : base(pizza) {
+        }
+
+        public override double Cost {
+            get {
+                var extraCost = Types.Contains(PizzaType.Mozzarella) ? 1 : 2;
+                return base.Cost + extraCost;
+            }
+        }
+
+        protected override PizzaType PizzaType { get { return PizzaType.Ham; } }
+    }
+
+    public enum PizzaType
+    {
+        Plain,
+        Mozzarella,
+        Ham
+    }
+}
---
+++ src/Patterns/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Patterns
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            IPizza pizza = new Ham(new Mozzeralla(new PlainPizza()));
+            Console.WriteLine(pizza.Description);
+            Console.WriteLine(pizza.Cost);
+
+            Console.ReadLine();
+        }
+    }
+}
